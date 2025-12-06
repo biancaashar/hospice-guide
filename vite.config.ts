9533f027a57a730ad7165bb8410f33d0417364a6
biancaashar <60717839+biancaashar@users.noreply.github.com>
2025-12-06 17:17:36 -0500

    Update vite.config.ts
    To allow render

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: ['hospice-guide.onrender.com'], // <--- added this line
       },
       plugins: [react()],
       define: {
@@ -21,3 +22,4 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+
